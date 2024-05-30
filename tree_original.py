30a4950562409b9f25a0b77ad6d3a2a343eb059f
Aliceliao31 <alice90131@gmail.com>
2024-05-31 00:11:48 +0800

    Add files via upload
    save animation

--- tree_original.py
+++ tree_original.py
@@ -1,47 +1,66 @@
 import math
-<<<<<<< Updated upstream
-=======
+import numpy as np
 import matplotlib.pyplot as plt
-import cython
->>>>>>> Stashed changes
+import matplotlib.animation as animation
 
 class Body:
     def __init__(self, x, y, mass):
         self.x = x
         self.y = y
         self.mass = mass
-        self.vx = 0
-        self.vy = 0
+        self.vx = 1
+        self.vy = 0.5
         self.ax = 0
         self.ay = 0
 
-class QuadtreeNode:
-    def __init__(self, x_min, x_max, y_min, y_max):
-        self.x_min = x_min
-        self.x_max = x_max
-        self.y_min = y_min
-        self.y_max = y_max
-        self.body = None
-        self.mass = 0
-        self.center_of_mass_x = 0
-        self.center_of_mass_y = 0
-        self.children = [None, None, None, None]
+class rect:
+    def __init__(self, x, y, h, w):
+        self.x = x
+        self.y = y
+        self.h = h
+        self.w = w
 
-    def is_leaf(self):
-        return all(child is None for child in self.children)
+class QuadtreeNode:
+    def __init__(self, boundary, level):
+        self.max_obj = 1
+        self.body = None
+        self.area = []
+        self.level = level
+        self.boundary = boundary
+    
+    def check_boundary(self, body):
+        x_b, y_b = body.x, body.y
+        x, y, h, w = self.boundary.x, self.boundary.y, self.boundary.h, self.boundary.w
+        if self.level != 0:
+            if x_b < x or x_b > x+w or y_b < y or y_b > y+h:
+                return True
 
     def insert(self, body):
-        if self.is_leaf():
-            if self.body is None:
-                self.body = body
-            else:
-                self.subdivide()
-                self._insert_into_children(self.body)
-                self._insert_into_children(body)
-                self.body = None
-        else:
-            self._insert_into_children(body)
-        self._update_mass_distribution()
+        if self.check_boundary(body):
+            return False
+        if self.body == None:
+            self.body = body
+            return True
+        if len(self.area) <= 0:
+            self.subdivide()
+            for area_ in self.area:
+                if area_.insert(self.body):
+                    self.body = "empty"
+                    break
+        for area_ in self.area:
+            if area_.insert(body):
+                return True
+
+    def subdivide(self):
+        x = self.boundary.x
+        y = self.boundary.y
+        h = self.boundary.h
+        w = self.boundary.w
+
+        self.area.append(QuadtreeNode(rect(x+w/2, y, h/2, w/2), self.level+1))
+        self.area.append(QuadtreeNode(rect(x, y, h/2, w/2), self.level+1))
+        self.area.append(QuadtreeNode(rect(x, y+h/2, h/2, w/2), self.level+1))
+        self.area.append(QuadtreeNode(rect(x+w/2, y+h/2, h/2, w/2), self.level+1))
 
     def _insert_into_children(self, body):
         for i, (x_min, x_max, y_min, y_max) in enumerate(self._get_quadrants()):
@@ -61,104 +80,126 @@ class QuadtreeNode:
             (x_mid, self.x_max, y_mid, self.y_max),
         ]
 
+    def calculate_center_of_mass(self, mass = 0, center_of_mass_x = 0, center_of_mass_y = 0):
+        if self.body == "empty":
+            for area_ in self.area:
+                mass, center_of_mass_x, center_of_mass_y = area_.calculate_center_of_mass(mass, center_of_mass_x, center_of_mass_y)
+                
+        elif self.body != None:
+            mass += self.body.mass
+            center_of_mass_x += self.body.mass*self.body.x
+            center_of_mass_y += self.body.mass*self.body.y
+
+
+        return mass, center_of_mass_x, center_of_mass_y
+
     def _update_mass_distribution(self):
         self.mass = 0
-        self.center_of_mass_x = 0
-        self.center_of_mass_y = 0
-        if self.is_leaf():
-            if self.body:
-                self.mass = self.body.mass
-                self.center_of_mass_x = self.body.x
-                self.center_of_mass_y = self.body.y
-        else:
-            for child in self.children:
-                if child:
-                    self.mass += child.mass
-                    self.center_of_mass_x += child.mass * child.center_of_mass_x
-                    self.center_of_mass_y += child.mass * child.center_of_mass_y
-            if self.mass > 0:
+        self.center_of_mass_x = None
+        self.center_of_mass_y = None
+        if self.body != None:
+            self.mass, self.center_of_mass_x, self.center_of_mass_y = self.calculate_center_of_mass()
+            if self.mass != 0:
                 self.center_of_mass_x /= self.mass
                 self.center_of_mass_y /= self.mass
+            for area_ in self.area:
+                area_._update_mass_distribution()
+    
+    def plot(self, ax):
+        x = self.boundary.x
+        y = self.boundary.y
+        h = self.boundary.h
+        w = self.boundary.w
+        ax.plot([x, x], [y+h, y], color="black")
+        ax.plot([x, x+w], [y, y], color="black")
+        ax.plot([x+w, x+w], [y+h, y], color="black")
+        ax.plot([x, x+w], [y+h, y+h], color="black")
+        if self.center_of_mass_x != None:
+            ax.plot([self.center_of_mass_x],[self.center_of_mass_y], "gx")
+        if len(self.area) != 0:
+            for area_ in self.area:
+                area_.plot(ax)
+        if self.body != None and self.body != "empty":
+            ax.plot([self.body.x],[self.body.y],"bo", ms=5)
 
-def compute_force(body, node, theta=0.5, G=6.67430e-11):
-    if node.is_leaf():
+def compute_force(body, node, theta=0.5, G=1):
+    if node.body != "empty":
         if node.body is not None and node.body != body:
             dx = node.body.x - body.x
             dy = node.body.y - body.y
             distance = math.sqrt(dx**2 + dy**2)
             if distance > 0:
                 force = G * body.mass * node.body.mass / (distance**2)
-                body.ax += force * dx / distance
-                body.ay += force * dy / distance
+                body.ax += force * dx / (distance*body.mass)
+                body.ay += force * dy / (distance*body.mass)
     else:
         dx = node.center_of_mass_x - body.x
         dy = node.center_of_mass_y - body.y
         distance = math.sqrt(dx**2 + dy**2)
-        size = node.x_max - node.x_min
+        size = node.boundary.w
         if size / distance < theta:
             force = G * body.mass * node.mass / (distance**2)
-            body.ax += force * dx / distance
-            body.ay += force * dy / distance
+            body.ax += force * dx / (distance*body.mass)
+            body.ay += force * dy / (distance*body.mass)
         else:
-            for child in node.children:
-                if child:
-                    compute_force(body, child, theta, G)
-<<<<<<< Updated upstream
-=======
-
-def move(body, node, theta=0.5, G=6.67430e-11):  
-#####   update orbit (drift-kick-drift (DKD))  #####
-#     (a) drift: update position by 0.5*dt
+            for area_ in node.area:
+                compute_force(body, area_, theta, G)
+def update_per_body(body, node):
+    ##### update orbit (DKD) ######
+    # drift
     body.x = body.x + body.vx*0.5*dt
     body.y = body.y + body.vy*0.5*dt
 
-#     (b) kick: calculate a(t+0.5*dt) and use that to update velocity by dt
-    r     = ( x*x + y*y )**0.5
-    a_abs = G*M/(r*r)
-    ax    = -a_abs*x/r
-    ay    = -a_abs*y/r
-    vx    = vx + ax*dt
-    vy    = vy + ay*dt
+    # kick
+    compute_force(body, node)
+    body.vx = body.vx + body.ax*dt
+    body.vy = body.vy + body.ay*dt
 
-#     (c) drift: use v(t+dt) to update position by another 0.5*dt
+    # drift
     body.x = body.x + body.vx*0.5*dt
     body.y = body.y + body.vy*0.5*dt
+    return body
 
-#     update time
-    #t = t + dt
-    #if ( t >= end_time ):   
-     #   break
-
-    # if node.is_leaf():
-    #     if node.body is not None and node.body != body:
-    #         dx = node.body.x - body.x
-    #         dy = node.body.y - body.y
-    #         distance = math.sqrt(dx**2 + dy**2)
-    #         if distance > 0:
-    #             force = G * body.mass * node.body.mass / (distance**2)
-    #             body.ax += force * dx / distance
-    #             body.ay += force * dy / distance
-    # else:
-    #     dx = node.center_of_mass_x - body.x
-    #     dy = node.center_of_mass_y - body.y
-    #     distance = math.sqrt(dx**2 + dy**2)
-    #     size = node.x_max - node.x_min
-    #     if size / distance < theta:
-    #         force = G * body.mass * node.mass / (distance**2)
-    #         body.ax += force * dx / distance
-    #         body.ay += force * dy / distance
-    #     else:
-    #         for child in node.children:
-    #             if child:
-    #                 compute_force(body, child, theta, G)
+def update_per_dt(frame):
+    ax.clear()
+    ax.set_xlim([-100,100])
+    ax.set_ylim([-100,100])
+    x_list = []
+    y_list = []
+    for body in body_list:
+        qt.insert(body)
+    qt._update_mass_distribution()
+    for i in range(len(body_list)):
+        body_list[i] = update_per_body(body_list[i], qt)
+        x_list.append(body_list[i].x)
+        y_list.append(body_list[i].y)
+    print(body_list[1].x)
+    ax.plot(x_list, y_list, "bo", ms=5)
     
->>>>>>> Stashed changes
+def init():
+    x_list = []
+    y_list = []
+    for body in body_list:
+        x_list.append(body.x)
+        y_list.append(body.y)
+    ax.plot(x_list, y_list, "bo", ms=5)
 
-# Example usage:
-bodies = [Body(x, y, mass) for x, y, mass in [(0, 0, 1), (1, 0, 1), (0, 1, 1), (1, 1, 1)]]
-root = QuadtreeNode(-2, 2, -2, 2)
-for body in bodies:
-    root.insert(body)
-for body in bodies:
-    compute_force(body, root)
-    print(f"Body at ({body.x}, {body.y}) has acceleration ({body.ax}, {body.ay})")
+if __name__ == "__main__":
+    t = 0.0
+    dt = 1e-1
+    end_time = 1.0
+    body_list = [Body(-0.9,0.9,1),Body(-1,-1,1),Body(1,1,1),Body(1,-1,1),Body(-0.8,0.5,1),Body(-0.5,0.8,1),
+                 Body(-0.4,0.7,1),Body(-1.5,0.5,1),Body(-1.5,0.3,1)]
+#    body_list = [Body(-5.0,3.0,1),Body(7.3,-4.0,1),Body(-7.9,-2.6,1),Body(1,-1,1),Body(-0.8,0.5,1),Body(-0.5,0.8,1),
+#                 Body(-0.4,0.7,1),Body(-1.5,0.5,1),Body(-1.5,0.3,10)]
+    fig, ax = plt.subplots()
+    qt = QuadtreeNode(rect(-10,-10,20,20),0)
+    nframe = int( np.ceil( end_time/dt ) )
+    anim   = animation.FuncAnimation( fig, func=update_per_dt, init_func=init,
+                                  frames=nframe, interval=10, repeat=False )
+
+    plt.show()
+    ###save the animation
+    # animation = camera.animate()
+    # animation.save('test tree.gif', writer = 'pillow', fps=40)
+    animation.save("test1.gif", writer='pillow') #The default is in the project, you can also specify the path. 
